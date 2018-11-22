e67731b7da0eb46d39152a73ae0ba499f75c74a6
SegFault42 <SegFault42@protonmail.com>
2018-11-22 15:35:39 +0700

    improove ...

---
+++ i3_battery_alert.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/distatus/battery"
+	"github.com/mqu/go-notify"
+)
+
+type notifAlert struct {
+	twentyPercent bool
+	tenPercent    bool
+	fivePercent   bool
+}
+
+func getBatteryLevel() (uint8, error) {
+
+	var batteryLevel uint8 = 0
+
+	// Get battery information
+	battery, err := battery.Get(0)
+	if err != nil {
+		fmt.Println("Could not get battery info!")
+		return batteryLevel, err
+	}
+
+	// calculate percentage
+	batteryLevel = uint8((battery.Current / battery.Full) * 100)
+
+	return batteryLevel, err
+}
+
+func main() {
+
+	var (
+		notif *notify.NotifyNotification = nil
+		alert notifAlert
+	)
+
+	notify.Init("Low Battery !")
+
+	for {
+		// Get battery level in percent
+		batteryLevel, err := getBatteryLevel()
+		if err != nil {
+			return
+		}
+
+		// make a popup
+		if batteryLevel == 20 && alert.twentyPercent == false {
+			notif = notify.NotificationNew("Low Battery !", "Battery level: "+strconv.Itoa(int(batteryLevel))+"%", "dialog-information")
+			notif.SetUrgency(notify.NOTIFY_URGENCY_CRITICAL)
+			alert.twentyPercent = true
+		} else if batteryLevel == 10 && alert.tenPercent == false {
+			notif = notify.NotificationNew("Low Battery !", "Battery level: "+strconv.Itoa(int(batteryLevel))+"%", "dialog-information")
+			notif.SetUrgency(notify.NOTIFY_URGENCY_CRITICAL)
+			alert.tenPercent = true
+		} else if batteryLevel == 5 && alert.fivePercent == false {
+			notif = notify.NotificationNew("Low Battery !", "Battery level: "+strconv.Itoa(int(batteryLevel))+"%", "dialog-information")
+			notif.SetUrgency(notify.NOTIFY_URGENCY_CRITICAL)
+			alert.fivePercent = true
+		}
+
+		// Show popup
+		if notif != nil {
+			notif.Show()
+			notif = nil
+		}
+	}
+}
