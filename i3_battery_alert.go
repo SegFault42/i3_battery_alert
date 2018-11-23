4f82f8b64b367099c0b8eed37b5f96fa89363907
SegFault42 <SegFault42@protonmail.com>
2018-11-23 08:39:31 +0700

    refactor main(), add .gitignore

--- i3_battery_alert.go
+++ i3_battery_alert.go
@@ -31,6 +31,38 @@ func getBatteryLevel() (uint8, error) {
 	return batteryLevel, err
 }
 
+func resetVar(alert notifAlert, batteryLevel uint8) {
+
+	if batteryLevel > 20 {
+		alert.twentyPercent = false
+	}
+	if batteryLevel > 10 {
+		alert.tenPercent = false
+	}
+	if batteryLevel > 5 {
+		alert.fivePercent = false
+	}
+}
+
+func setNotification(alert *notifAlert, batteryLevel uint8) (notif *notify.NotifyNotification) {
+
+	if batteryLevel == 20 && alert.twentyPercent == false {
+		notif = notify.NotificationNew("Low Battery !", "Battery level: "+strconv.Itoa(int(batteryLevel))+"%", "dialog-information")
+		notif.SetUrgency(notify.NOTIFY_URGENCY_CRITICAL)
+		alert.twentyPercent = true
+	} else if batteryLevel == 10 && alert.tenPercent == false {
+		notif = notify.NotificationNew("Low Battery !", "Battery level: "+strconv.Itoa(int(batteryLevel))+"%", "dialog-information")
+		notif.SetUrgency(notify.NOTIFY_URGENCY_CRITICAL)
+		alert.tenPercent = true
+	} else if batteryLevel == 5 && alert.fivePercent == false {
+		notif = notify.NotificationNew("Low Battery !", "Battery level: "+strconv.Itoa(int(batteryLevel))+"%", "dialog-information")
+		notif.SetUrgency(notify.NOTIFY_URGENCY_CRITICAL)
+		alert.fivePercent = true
+	}
+
+	return
+}
+
 func main() {
 
 	var (
@@ -48,35 +80,17 @@ func main() {
 		}
 
 		// Reset var if battery level charge up to limit (20, 10 or 5)
-		if batteryLevel > 20 {
-			alert.twentyPercent = false
-		}
-		if batteryLevel > 10 {
-			alert.tenPercent = false
-		}
-		if batteryLevel > 5 {
-			alert.fivePercent = false
-		}
+		resetVar(alert, batteryLevel)
 
-		// make a popup
-		if batteryLevel == 20 && alert.twentyPercent == false {
-			notif = notify.NotificationNew("Low Battery !", "Battery level: "+strconv.Itoa(int(batteryLevel))+"%", "dialog-information")
-			notif.SetUrgency(notify.NOTIFY_URGENCY_CRITICAL)
-			alert.twentyPercent = true
-		} else if batteryLevel == 10 && alert.tenPercent == false {
-			notif = notify.NotificationNew("Low Battery !", "Battery level: "+strconv.Itoa(int(batteryLevel))+"%", "dialog-information")
-			notif.SetUrgency(notify.NOTIFY_URGENCY_CRITICAL)
-			alert.tenPercent = true
-		} else if batteryLevel == 5 && alert.fivePercent == false {
-			notif = notify.NotificationNew("Low Battery !", "Battery level: "+strconv.Itoa(int(batteryLevel))+"%", "dialog-information")
-			notif.SetUrgency(notify.NOTIFY_URGENCY_CRITICAL)
-			alert.fivePercent = true
-		}
+		// set a popup
+		notif = setNotification(&alert, batteryLevel)
 
 		// Show popup
 		if notif != nil {
 			notif.Show()
 			notif = nil
 		}
+
+		//time.Sleep(60 * time.Second)
 	}
 }
